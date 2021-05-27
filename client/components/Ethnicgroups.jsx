95cc5427f07f2755d5ee7070ba627b0f11d0b1f8
cherrycrush <phobosnz@gmail.com>
2021-05-27 12:45:55 +1200

    Components rendering on App

---
+++ client/components/Ethnicgroups.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Ethnicgroups () {
+  return (
+    <>
+      <div className='ethnicgroups'>
+        <h1 className='heading'>Our ethnic groups</h1>
+        <p>For the census usually resident population count</p>
+        <p>Of the 100 people in our village...</p>
+      </div>
+    </>
+  )
+}
---
+++ client/components/Languages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Languages () {
+  return (
+    <>
+      <div className='languages'>
+        <h1 className='heading'>Languages we use</h1>
+        <p>For the census usually resident population count</p>
+        <p>Of the 100 people in our village...</p>
+      </div>
+    </>
+  )
+}
