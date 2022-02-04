e7abd00df3f02ed566cb678a534d051266e386fa
cherrycrush <phobosnz@gmail.com>
2022-02-05 11:09:44 +1300

    Flex: description

--- client/components/Birthplace.jsx
+++ client/components/Birthplace.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import Flip from 'react-reveal/Flip'
+import Slide from 'react-reveal/Slide'
 
 export default function Birthplace () {
   return (
     <>
       <div className='birthplace'>
-        <Flip>
+        <Flip delay={9000}>
           <h1 className='heading'>Birthplace</h1>
           <p>Of the 100 people in our village...</p>
         </Flip>
-        <div className='container'>
-          <div className='nz'>73</div>
-          <div className='overseas'>27</div>
-        </div>
-        <div className='description'>
-          <p>were born in New Zealand</p>
-          <p>were born overseas</p>
-        </div>
-        <div className='map'>
-          <h1 className='heading'>Top 5 overseas birthplaces</h1>
-          <p>For the census usually resident population count</p>
-          <p>Of the 100 people in our village...</p>
-        </div>
+        <Slide delay={10000}>
+          <div className='container'>
+            <div className='nz'>73</div>
+            <div className='overseas'>27</div>
+          </div>
+          <div className='description'>
+            <p>were born in New Zealand</p>
+            <p>were born overseas</p>
+          </div>
+          <div className='map'>
+            <h1 className='heading'>Top 5 overseas birthplaces</h1>
+            <p>For the census usually resident population count</p>
+            <p>Of the 100 people in our village...</p>
+          </div>
+        </Slide>
       </div>
     </>
   )
